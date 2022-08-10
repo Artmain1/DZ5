84a30ef5c7a0d5942606c5987dde5deed24693fc
Artur <89503357000@ya.ru>
2022-08-10 23:12:30 +0600

    Main

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+456 -> 5
+782 -> 8
+918 -> 1*/
+Console.Write("Введи трехкратное число:");
+int number = Convert.ToInt16(Console.ReadLine());
+
+if(number > 100 && number < 999)
+{
+    number = (number % 100 / 10);
+    Console.WriteLine(number);
+}
+else
+{
+    Console.WriteLine("Введена не правильная цифра");
+}
